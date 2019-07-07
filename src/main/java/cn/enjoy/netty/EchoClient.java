4a798a54e2502be12bdd6af61e93e4e94e058414
刘志华 <2509687281@qq.com>
2019-07-07 17:03:16 +0800

    新增服务端

--- src/main/java/cn/enjoy/netty/EchoClient.java
+++ src/main/java/cn/enjoy/netty/EchoClient.java
@@ -27,19 +27,25 @@ public class EchoClient {
     public void start()throws InterruptedException{
         //EventLoopGroup可以理解为线程租
         EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+
 //启动必须，做一些初始化工作
-        Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(eventLoopGroup)//把线程组传入
-                //指定使用nio进行网络通信
-                .channel(NioSocketChannel.class)
-                /*链接端口ip建立链接*/
-               .remoteAddress(new InetSocketAddress(host,port))
-                //入站处理器
-               .handler(new EchoClientHabdler());
-               //链接并且阻塞，直到链接完成
-        ChannelFuture f = bootstrap.connect().sync();
-        //阻塞直到发生关闭事件
-        f.channel().closeFuture().sync();
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup)//把线程组传入
+                    //指定使用nio进行网络通信
+                    .channel(NioSocketChannel.class)
+                    /*链接端口ip建立链接*/
+                    .remoteAddress(new InetSocketAddress(host,port))
+                    //入站处理器
+                    .handler(new EchoClientHabdler());
+            //链接并且阻塞，直到链接完成
+            ChannelFuture f = bootstrap.connect().sync();
+            //阻塞直到发生关闭事件
+            f.channel().closeFuture().sync();
+
+        }finally {
+            eventLoopGroup.shutdownGracefully();
+        }
 
     }
     public static void main(String[] args) throws InterruptedException {
